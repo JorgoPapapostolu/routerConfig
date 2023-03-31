79b9ebfe9f1dc690411cc09ecc2461e97312a58a
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2023-03-31 13:22:39 +0200

    create a user

--- controller/user.ts
+++ controller/user.ts
@@ -1,6 +1,7 @@
 import pool from '../db';
 import { Request, Response } from 'express';
 
+
 export const getUsers = async (req: Request, res: Response) => {
     try {
         const { rows } = await pool.query('SELECT * from users');
@@ -25,3 +26,16 @@ export const getUsersById = async (req: Request, res: Response) => {
         res.status(500).send('Internal Server Error');
     }
 };
+
+export const postUser = async (req: Request, res: Response) => {
+    const { id, firstname, lastname  } = req.body;
+    try {
+      const { rows } = await pool.query(
+        'INSERT INTO users (id, firstname, lastname) VALUES ($1, $2, $3) RETURNING *',
+        [id, firstname, lastname]
+      );
+      res.json({ data: rows });
+    } catch (err) {
+      res.status(500).send('Internal Server Error');
+    }
+  };
--- routes/user.ts
+++ routes/user.ts
@@ -1,6 +1,23 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from "express";
 export const userRouter: Router = Router();
-import { getUsers, getUsersById } from '../controller/user';
+import { getUsers, getUsersById, postUser } from "../controller/user";
+import { body, validationResult } from "express-validator";
 
-userRouter.get('/', getUsers);
-userRouter.get('/:id', getUsersById);
+userRouter.get("/", getUsers);
+
+userRouter.get("/:id", getUsersById);
+
+userRouter.post(
+  "/",
+  body("id").isNumeric(),
+  body("firstname").isLength({ min: 2 }),
+  body("lastname").isLength({ min: 2 }),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors });
+    }
+    next();
+  },
+  postUser
+);
