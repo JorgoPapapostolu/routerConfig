dbecbb2e21d2175f09ecc91ca5e004bb5ec424df
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2023-03-31 13:51:51 +0200

    update user

--- controller/user.ts
+++ controller/user.ts
@@ -1,41 +1,58 @@
-import pool from '../db';
-import { Request, Response } from 'express';
-
+import pool from "../db";
+import { Request, Response } from "express";
 
 export const getUsers = async (req: Request, res: Response) => {
-    try {
-        const { rows } = await pool.query('SELECT * from users');
-        res.json({ data: rows })
-    } catch(err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const { rows } = await pool.query("SELECT * from users");
+    res.json({ data: rows });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 export const getUsersById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    try {
-        const { rows } = await pool.query('SELECT * from users WHERE id=$1', [id]);
-        if (rows.length > 0) {
-            res.json({ data: rows });
-        } else {
-            res.status(404).send('User not found');
-        }
-    } catch(err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query("SELECT * from users WHERE id=$1", [id]);
+    if (rows.length > 0) {
+      res.json({ data: rows });
+    } else {
+      res.status(404).send("User not found");
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 export const postUser = async (req: Request, res: Response) => {
-    const { id, firstname, lastname  } = req.body;
-    try {
-      const { rows } = await pool.query(
-        'INSERT INTO users (id, firstname, lastname) VALUES ($1, $2, $3) RETURNING *',
-        [id, firstname, lastname]
-      );
-      res.json({ data: rows });
-    } catch (err) {
-      res.status(500).send('Internal Server Error');
+  const { id, firstname, lastname } = req.body;
+  try {
+    const { rows } = await pool.query(
+      "INSERT INTO users (id, firstname, lastname) VALUES ($1, $2, $3) RETURNING *",
+      [id, firstname, lastname]
+    );
+    res.json({ data: rows });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { firstname, lastname } = req.body;
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
     }
-  };
+    const result = await pool.query(
+      "UPDATE users SET firstname=$1, lastname=$2 WHERE id=$3 RETURNING *",
+      [firstname, lastname, id]
+    );
+    res.json({ data: result.rows });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+};
--- routes/user.ts
+++ routes/user.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response, NextFunction } from "express";
 export const userRouter: Router = Router();
-import { getUsers, getUsersById, postUser } from "../controller/user";
 import { body, validationResult } from "express-validator";
+import {
+  getUsers,
+  getUsersById,
+  postUser,
+  updateUser,
+} from "../controller/user";
 
 userRouter.get("/", getUsers);
 
@@ -20,4 +25,19 @@ userRouter.post(
     next();
   },
   postUser
+);
+
+userRouter.put(
+  "/:id",
+  body("id").isNumeric(),
+  body("firstname").isLength({ min: 2 }),
+  body("lastname").isLength({ min: 2 }),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors });
+    }
+    next();
+  },
+  updateUser
 );
